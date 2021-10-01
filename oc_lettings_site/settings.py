3a75a47d3a5cab2e32f0af6fc802163dd3fc54bf
jaikko <65661383+jaikko@users.noreply.github.com>
2021-10-01 15:54:37 +0200

    try fix bug missing staticfiles

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -138,7 +138,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/'),
 STATIC_URL = '/static/'
 # MEDIA_URL = '/media/'
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
@@ -151,6 +151,5 @@ DEBUG_PROPAGATE_EXCEPTIONS = True
 COMPRESS_ENABLED = False
 
 # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 django_heroku.settings(locals())
